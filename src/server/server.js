4226af10d4db5a12c2f583a2e66ac068e8b7f24b
adolzycki <a.dolzycki@gmail.com>
2019-06-29 18:12:10 +0200

    poprawa portu

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.use('public',express.static(__dirname + "/public"));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/Main.html')));
